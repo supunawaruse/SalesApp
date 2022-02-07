5e4850727be8f6c6c211e7a15de1b7d5a0a8ec5f
Supuna <supunawa@gmail.com>
2022-02-07 11:16:22 +0530

    main dependencies added

--- SalesApp/android/app/src/main/java/com/salesapp/MainApplication.java
+++ SalesApp/android/app/src/main/java/com/salesapp/MainApplication.java
@@ -1,5 +1,6 @@
 package com.salesapp;
-
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -32,6 +33,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage();
         }
       };
 
